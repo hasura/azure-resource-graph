1d772439b102a872420511e7bf6d1682e3e257c3
kenstott <128912107+kenstott@users.noreply.github.com>
2025-06-21 16:26:06 -0400

    Bump version: 1.0.11 → 1.0.12

--- azure_resource_graph/__init__.py
+++ azure_resource_graph/__init__.py
@@ -8,7 +8,7 @@ storage encryption compliance across applications using the Azure Resource Graph
 
 from .client import AzureResourceGraphClient, AzureConfig
 
-__version__ = "1.0.11"
+__version__ = "1.0.12"
 __author__ = "Kenneth Stott"
 __email__ = "ken@hasura.io"
 
